fe2e95900042f3244f16c691bae30a80265aad8d
StevenProgrammer01 <91440171+StevenProgrammer01@users.noreply.github.com>
2023-04-15 19:14:36 -0600

    change the mongo uri

--- bd/client.py
+++ bd/client.py
@@ -4,4 +4,4 @@ from pymongo import MongoClient
 
 #Base de datos remota
 
-db_client = MongoClient("mongodb+srv://stevendevprz:holamundo123@cluster0.dtffzvo.mongodb.net/?retryWrites=true&w=majority").test
+db_client = MongoClient("mongodb+srv://stevendevprz:holamundo123@cluster0.ci5jdhj.mongodb.net/?retryWrites=true&w=majority").test
