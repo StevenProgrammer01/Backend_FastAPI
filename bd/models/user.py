0f05a7d219f0eef3a413f4cac5bfd75fc61d2b6e
StevenProgrammer01 <91440171+StevenProgrammer01@users.noreply.github.com>
2023-04-15 20:01:54 -0600

    other configurations

--- bd/models/user.py
+++ bd/models/user.py
@@ -1,6 +1,10 @@
 from typing import Optional
 from pydantic import BaseModel
 class User(BaseModel):
+    #id: str
+    username: str
+    email:str
+class User2(BaseModel):
     id: str
     username: str
     email:str
--- routers/users_db.py
+++ routers/users_db.py
@@ -8,7 +8,7 @@ pydantic: Librería para poder generar BaseModel, y crear objetos JSON a partir 
 
 '''
 from fastapi import APIRouter, HTTPException 
-from bd.models.user import User
+from bd.models.user import User,User2
 from bd.client import db_client
 from bd.schemas.user import user_schema, users_schema
 from bson import ObjectId
@@ -23,33 +23,33 @@ users_list = []
 async def users():
     return users_schema(db_client.users.find())
 
-@router.get("/{id}",response_model=User)
+@router.get("/{id}",response_model=User2)
 
 async def user(id:str):
     user = search_user("_id",ObjectId(id))
-    if type(user) == User:
+    if type(user) == User2:
         return user
     else:
         raise HTTPException(status_code=400, detail="That user is not exist")
 
 
-@router.post("/",status_code=201, response_model=User) 
+@router.post("/",status_code=201, response_model=User2) 
 async def add_user(user: User):
     if type(search_user("email",user.email))==User:
         raise HTTPException(404,"That user is already exists")
     
     user_dict = dict(user)
-    del user_dict["id"]
+    
     #Insertar un registro en mongo db
     id = db_client.users.insert_one(user_dict).inserted_id #Este a su vez lo crea y devuelve su id
     #Buscamos un registro por su id
 
     #User_schema es una función en la que podremos manipular los campos del registro que nos devuelve mongodb
     n_user = user_schema(db_client.users.find_one({"_id":id}))
-    return User(**n_user)  
+    return User2(**n_user)  
     
-@router.put("/",response_model=User,status_code=202)
-async def update_user(user:User):
+@router.put("/",response_model=User2,status_code=202)
+async def update_user(user:User2):
     user_dict = dict(user)
     del user_dict["id"]
     try:
@@ -70,7 +70,7 @@ async def delete_user(id:str):
 def search_user(key:str, value):
     try:
        user = user_schema(db_client.users.find_one({key:value}))
-       return User(**user)
+       return User2(**user)
     except:
         #raise HTTPException(status_code=404,detail="That user is not exist")
         return None
